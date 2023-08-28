48d82b136488f19986ffb9fe23c7d1e8bd3979ce
Muhammad Sultan Al Mahfuz <60539168+mahfuz225bd@users.noreply.github.com>
2023-08-28 20:33:08 +0600

    .

---
+++ Day 2 - 28 Aug/UserInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInput {
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		System.out.print("What is you name?: ");
+		
+		String name;
+		name = myScanner.nextLine();
+
+		// Output: Welcome, {Inputed name}
+		System.out.println("Welcome, " + name);
+	}
+}
