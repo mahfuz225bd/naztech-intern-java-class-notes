73bd516277392e23ea0ce01256dfbe1fff45ef70
Muhammad Sultan Al Mahfuz <60539168+mahfuz225bd@users.noreply.github.com>
2023-09-14 23:56:30 +0600

    added first program of 13th Sep, 2023

---
+++ Day 10 - 13 Sep - Java/MainProgram.java
@@ -0,0 +1,7 @@
+class MainProgram {
+    public static void main(String[] args) {
+	VariableInClass vic = new VariableInClass();
+	System.out.println("x=" + vic.x);
+	System.out.println("y=" + vic.y);	
+    }
+}
---
+++ Day 10 - 13 Sep - Java/VariableInClass.java
@@ -0,0 +1,6 @@
+public class VariableInClass {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+	}
+}
