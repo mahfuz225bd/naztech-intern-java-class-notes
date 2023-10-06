5d98d08cd196efc6002d872c21e0032d39963b4e
Muhammad Sultan Al Mahfuz <60539168+mahfuz225bd@users.noreply.github.com>
2023-10-06 23:12:43 +0600

    upadted DataTypes.java + added pdf files

--- class_notes/Day 03 - 29 Aug - Java/DataTypes.java
+++ class_notes/Day 03 - 29 Aug - Java/DataTypes.java
@@ -1,45 +1,44 @@
 import java.util.Scanner;
 
-// This program explain all primitive datatypes.
-// 8 bit = 1 byte (Please, look at this.)
+// This program implemented all primitive data types to understand.
+// 8 bit = 1 byte
 
-public class Hello {
+public class DataTypes {
 
     public static void main(String[] args) {
     	Scanner dataCollect = new Scanner(System.in);	
 
-    	// Byte can collect one bytes data.
+    	// Size of Byte = 1 byte
     	System.out.print("Enter your 'Byte' number: ");
     	byte byteNumber = dataCollect.nextByte(); 
     	System.out.println("your 'Byte' number is " + byteNumber + ".");
     	
-    	// Char can collect two bytes data.
-    	// At a time char can collect only one letter.
+    	// Size of Char = 2 bytes, which can contain single character of a string
     	System.out.print("\nEnter your 'Char' letter: ");
     	char charLetter = dataCollect.next().charAt(0); 
     	System.out.println("your 'Char' letter is " + charLetter + ".");
     	
-    	// Short can collect two bytes data.
+    	// Size of Short = 2 bytes
     	System.out.print("\nEnter your 'Short' number: ");
     	short shortNumber = dataCollect.nextShort(); 
     	System.out.println("your 'Short' number is " + shortNumber + ".");
     	
-    	// Integer can collect four bytes data.
+    	// Size of Integer = 4 bytes
     	System.out.print("\nEnter your 'Integer' number: ");
     	int intNumber = dataCollect.nextInt(); 
     	System.out.println("your 'Integer' number is " + intNumber + ".");
     	
-    	// Long can collect eight bytes data.
+    	// Size of Long = 8 bytes
     	System.out.print("\nEnter your 'Long' number: ");
     	long longNumber = dataCollect.nextLong(); 
     	System.out.println("your 'Long' number is " + longNumber + ".");
     	
-    	// Float can collect four bytes data.
+    	// Size of Float = 4 bytes
     	System.out.print("\nEnter your 'Float' number: ");
     	float floatNumber = dataCollect.nextFloat(); 
     	System.out.println("your 'Float' number is " + floatNumber + ".");
     	
-    	// Double can collect eight bytes data.
+    	// Size of Double = 8 bytes
     	System.out.print("\nEnter your 'Double' number: ");
     	double doubleNumber = dataCollect.nextDouble();
     	System.out.println("your 'Double' number is " + doubleNumber + ".");
