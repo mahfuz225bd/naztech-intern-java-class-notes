98b4697ef856e4d3884c86098b8f0ac0e9ff54e9
Muhammad Sultan Al Mahfuz <60539168+mahfuz225bd@users.noreply.github.com>
2023-10-14 23:47:41 +0600

    deleted unnecessary file and folders + fixed image urls

--- class_notes/Day 02 - 28 Aug - Java/CommandLineArg.java
+++
@@ -1,5 +0,0 @@
-class CommandLineArg {
-	public static void main(String[] args) {
-		System.out.println(args[0] + " " + args[1] + " " + args[2]);
-	}
-}
--- class_notes/Day 07 - 12 Sep - Java/VariableInClass.java
+++
@@ -1,4 +0,0 @@
-public class VariableInClass {
-	int x = 10;
-	int y = 20;
-}
